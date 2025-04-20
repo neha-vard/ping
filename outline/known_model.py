2e5809b991a4017eb8f649d9d43c1e2e93c1c7e3
Anya Parekh <anzparekh@gmail.com>
2025-04-20 18:19:00 -0500

    Replace local Deepface functionality with calls to GC functions

--- outline/known_model.py
+++ outline/known_model.py
@@ -1,22 +1,19 @@
-import os
-from deepface import DeepFace
+import base64
+import requests
 
 def predict(img_path):
     """Model prediction using DeepFace Facenet model."""
     print(f"Simulating prediction for: {img_path}")
-    data_dir = "known_people_dataset"
-    results = DeepFace.find(img_path=img_path, db_path=data_dir, model_name='Facenet')
-    # os.remove(img_path)
+    with open(img_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode("utf-8")
+    url = "https://us-central1-cs437-deepface.cloudfunctions.net/run_deepface"
+    payload = {
+        "image_base64": image_base64
+    }
 
-    if results and not results[0].empty:
-        top_match = results[0].iloc[0]
-        match_path = top_match["identity"]
-        person_name = os.path.basename(os.path.dirname(match_path))
-        if float(top_match["distance"]) < 0.4:
-            print(top_match["distance"])
-            print(f"Prediction: {person_name}")
-            return person_name
-        else:
-            return "No matches found."
-    else:
+    response = requests.post(url, json=payload)
+    match = response.json()["match"]
+    if match == None:
         return "No matches found."
+    else:
+        return match
--- outline/server.py
+++ outline/server.py
@@ -1,10 +1,6 @@
-import base64
-from io import BytesIO
-from PIL import Image
 from flask import Flask
 from flask_socketio import SocketIO, emit
-import os
-from datetime import datetime
+import requests
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -30,19 +26,17 @@ def handle_image_bytes_upload(data):
         return
 
     try:
-        # Decode base64 to bytes
-        image_bytes = base64.b64decode(image_data)
-
-        # Convert bytes to an image file using PIL
-        image = Image.open(BytesIO(image_bytes))
-
-        # Save the image to disk (optional)
-        save_path = os.path.join("known_people_dataset", f"{name}", f"{datetime.now().timestamp()}.jpg")
-        os.makedirs(os.path.dirname(save_path), exist_ok=True)
-        image.save(save_path)
+        # Save the image to cloud
+        url = "https://us-central1-cs437-deepface.cloudfunctions.net/add_image"
+        payload = {
+            "person": name,
+            "image_base64": image_data
+        }
 
         # Emit result
-        emit('image_registration_result', {'message': f"{save_path}"}, broadcast=True)
+        response = requests.post(url, json=payload)
+        message = response.json()["message"]
+        emit('image_registration_result', {'message': f"{message}"}, broadcast=True)
 
     except Exception as e:
         print("Error processing image upload:", e)
